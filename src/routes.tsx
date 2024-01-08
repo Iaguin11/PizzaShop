b50c1ab8e3e01c1e7ea66d244693e0cd4a5ae970
Iago Novaes <iagonovaes19@gmail.com>
2024-01-07 22:06:27 -0300

    organizing-components

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,8 +2,9 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layout/app'
 import { AuthLayout } from './pages/_layout/auth'
-import { Dashboard } from './pages/app/dashboard'
+import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
+import { NotFound } from './pages/auth/404'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
 
@@ -11,6 +12,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: '/',
